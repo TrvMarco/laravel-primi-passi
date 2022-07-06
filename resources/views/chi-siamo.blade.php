926123f722d7cda814699c84db022e8f454acd30
TrvMarco <trevisanello.marco@gmail.com>
2022-07-06 17:20:34 +0200

    Aggiunto pagina collegata a home page

---
+++ resources/views/chi-siamo.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Chi siamo?</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam quo harum laborum iusto, reiciendis, facere ipsam voluptatum officia consequuntur iste rem vel nihil totam sunt iure labore, praesentium sapiente perferendis!</p>
+</body>
+</html>
